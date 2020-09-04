a7baddaa673f83798fecdd4b47d1ac4586f768cb
patryk-kawalarz <patryk.kawalarz@gmail.com>
2020-09-05 01:28:45 +0200

    Update scale.js

--- scale.js
+++ scale.js
@@ -1,9 +1,6 @@
 const scale = (num, inMin, inMax) => {
-  let outMin = 0;
-  let outMax = 100;
-  
   if (num < in_min) return 0;
   if (num > in_max) return 100;
 
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  return (num - inMin) * 100 / (inMax - inMin);
 }
